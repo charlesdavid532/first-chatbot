043d09dc46da287e8d3d5b3c03b51b3bd9842b62
charles531 <charlesdavid531@gmail.com>
2017-07-18 01:26:33 +0530

    Added echo bot

--- app.py
+++ app.py
@@ -2,8 +2,14 @@ import os, sys
 from flask import Flask, request
 from flask import jsonify
 from flask.ext.pymongo import PyMongo
+from pymessenger import Bot
+
 
 app = Flask(__name__)
+
+PAGE_ACCESS_TOKEN = "EAABlZAhiLCzsBAEPENnZC43ODWjX1X4VT43TBjHP8dx8WC7W6kqVRLiRz5AljcmkxSk1rfD2ZA4dDdE149D8JIurZBM67Afl6MRFyZBmqH55mTbJTSbHAjKlHSQrHGITB129ekYkdLqGb2ZBJnN7vyEH4HjgPiXzZAO0yW9wj3WXwZDZD"
+
+bot = Bot(PAGE_ACCESS_TOKEN)
 
 app.config['MONGO_DBNAME'] = 'sales_data'
 app.config['MONGO_URI'] = 'mongodb://charles:password@ds161042.mlab.com:61042/sales_data'
@@ -30,6 +36,25 @@ def handle_message():
     data = request.get_json()
     print(data)
     sys.stdout.flush()
+    if data['object'] == 'page':
+        for entry in data['entry']:
+            for messaging_event in entry['messaging']:
+
+                # IDs
+		sender_id = messaging_event['sender']['id']
+		recipient_id = messaging_event['recipient']['id']
+
+		if messaging_event.get('message'):
+                    # Extracting text message
+                    if 'text' in messaging_event['message']:
+                        messaging_text = messaging_event['message']['text']
+                    else:
+                        messaging_text = 'no text'
+
+		    # Echo
+                    response = messaging_text
+                    bot.send_text_message(sender_id, response)
+
     return "ok", 200 
 
 
