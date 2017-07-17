b6b4ce4921f9daa8cbc7f165cb29d0e3836bbef5
charles531 <charlesdavid531@gmail.com>
2017-07-18 00:09:15 +0530

    Hope this works

--- app.py
+++ app.py
@@ -3,15 +3,11 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def index():
-    return "OK!", 200
-
-@app.route('/', methods=['POST'])
-def postIndex():
-    return "Hello world!", 200
+    return 'OK!'
 
 
-if __name__ == '__main__':
-    app.run(debug=True, port=80)
+if __name__ == "__main__":
+    app.run()
     
