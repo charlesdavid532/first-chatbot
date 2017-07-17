578df2021c0f241a23cf6becaf8c26b89c49eeb2
charles531 <charlesdavid531@gmail.com>
2017-07-17 22:51:29 +0530

    Initial commit

---
+++ connect_to_mlab.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from flask import jsonify
+from flask.ext.pymongo import PyMongo
+
+app = Flask(__name__)
+
+app.config['MONGO_DBNAME'] = 'sales_data'
+app.config['MONGO_URI'] = 'mongodb://charles:password@ds161042.mlab.com:61042/sales_data'
+
+mongo = PyMongo(app)
+
+@app.route('/')
+def index():
+    return 'OK!'
+
+@app.route('/add')
+
+def add():
+    
+    sale = mongo.db.sales
+    sale.insert({'city' : 'Faridabad', 'date': '17-July', 'amount' : '1300'})
+    return 'Added Sales row'
+
+@app.route('/query')
+def query():
+    sale = mongo.db.sales
+    output = []
+    for s in sale.find({'city': 'Pune'}):
+        output.append({'city' : s['city'], 'date' : s['date'], 'amount': s['amount']})
+    return jsonify({'output':output})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
