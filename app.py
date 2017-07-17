64db905e9414ef2c14b5a5775302d4ad7525b9f8
charles531 <charlesdavid531@gmail.com>
2017-07-18 00:27:07 +0530

    Adding mongo code

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 from flask import Flask
+from flask import jsonify
+from flask.ext.pymongo import PyMongo
 
 app = Flask(__name__)
 
+app.config['MONGO_DBNAME'] = 'sales_data'
+app.config['MONGO_URI'] = 'mongodb://charles:password@ds161042.mlab.com:61042/sales_data'
+
+mongo = PyMongo(app)
 
 @app.route('/')
 def index():
     return 'Hello world!'
 
+@app.route('/add')
+
+def add():
+    
+    sale = mongo.db.sales
+    sale.insert({'city' : 'Faridabad', 'date': '17-July', 'amount' : '1300'})
+    return 'Added Sales row'
 
 if __name__ == "__main__":
     app.run()
