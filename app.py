a35aa72890c3d12f325c07c18f01d2f376a36bf3
charles531 <charlesdavid531@gmail.com>
2017-07-17 23:49:07 +0530

    Added few changes

--- app.py
+++ app.py
@@ -1,19 +1,17 @@
 from flask import Flask
-from flask import jsonify
-from flask.ext.pymongo import PyMongo
 
 app = Flask(__name__)
 
-app.config['MONGO_DBNAME'] = 'sales_data'
-app.config['MONGO_URI'] = 'mongodb://charles:password@ds161042.mlab.com:61042/sales_data'
-
-mongo = PyMongo(app)
 
 @app.route('/', methods=['GET'])
 def index():
-    return 'OK!'
+    return "OK!", 200
+
+@app.route('/', methods=['POST'])
+def postIndex():
+    return "Hello world!", 200
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=80)
     
