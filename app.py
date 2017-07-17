024a60b77e0845c75eae61e957adda72aec26218
charles531 <charlesdavid531@gmail.com>
2017-07-18 01:29:29 +0530

    Rolling back

--- app.py
+++ app.py
@@ -36,25 +36,6 @@ def handle_message():
     data = request.get_json()
     print(data)
     sys.stdout.flush()
-    if data['object'] == 'page':
-        for entry in data['entry']:
-            for messaging_event in entry['messaging']:
-
-                # IDs
-		sender_id = messaging_event['sender']['id']
-		recipient_id = messaging_event['recipient']['id']
-
-		if messaging_event.get('message'):
-                    # Extracting text message
-                    if 'text' in messaging_event['message']:
-                        messaging_text = messaging_event['message']['text']
-                    else:
-                        messaging_text = 'no text'
-
-		    # Echo
-                    response = messaging_text
-                    bot.send_text_message(sender_id, response)
-
     return "ok", 200 
 
 
