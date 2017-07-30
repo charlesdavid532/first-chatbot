1c8eb048fd7fb2d6ab972e0c96b4640c0d728fbc
charles531 <charlesdavid531@gmail.com>
2017-07-31 03:17:34 +0530

    Added clause for end date

--- app.py
+++ app.py
@@ -189,7 +189,7 @@ def queryDateForDateRange(datePeriod):
      
     for s in sale.find():
         print ("The date is:" + s['date'])
-        if dt.strptime(s['date'], "%Y-%m-%d") > dt.strptime(startDate, "%Y-%m-%d"):
+        if (dt.strptime(s['date'], "%Y-%m-%d") > dt.strptime(startDate, "%Y-%m-%d")) and (dt.strptime(s['date'], "%Y-%m-%d") < dt.strptime(endDate, "%Y-%m-%d")):
             print ("Inside if")
             amount = amount + int(s['amount'])
     if amount != None:
